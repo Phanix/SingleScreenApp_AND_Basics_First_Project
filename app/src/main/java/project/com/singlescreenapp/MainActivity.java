6a511f4ec3600bf7387b4d5bd22d9bd098774dae
fabio henrique da silva <hantarodev@gmailcom>
2019-06-09 20:06:48 -0300

    Initial commit

---
+++ app/src/main/java/project/com/singlescreenapp/MainActivity.java
@@ -0,0 +1,14 @@
+package project.com.singlescreenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Free vector art via <a href="https://www.vecteezy.com">Vecteezy</a>
+    }
+}
